09044f02d39b632f3b8bff203eb7af02fad77d61
HMDuran <hanahmae.duran@gmail.com>
2022-08-01 19:47:12 +0800

    update Exercise_2

--- Exercise_2/Exercise2.c
+++ Exercise_2/Exercise2.c
@@ -1,19 +1,35 @@
+/**
+ * @file Exercise2.c
+ * @author HMDuran
+ * @brief program will identify if the alphabet is vowel or constant.
+ * @version 0.1
+ * @date 2022-08-01
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+/* header files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
-int main ()
-{
-     char ch;
-     printf ("Enter an Alphabet: \n");
-     scanf ("%ch", &ch);
+int main() {
+  /* declaration of variables */
+  char letter;
 
-     if (   ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-        printf ("\n%c is a VOWEL.", ch);
-    }
-     else {
-        printf ("\n%c is a CONSONANT.", ch);
-    }
-    return 0;
+  /* asking user to enter an alphabet  */
+  printf("\nEnter an Alphabet: ");
+  scanf("%c", &letter);
+
+  /* evaluate if the variable letter is a vowel */
+  if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' ||
+      letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' ||
+      letter == 'O' || letter == 'U') {
+    printf("\n%c is a VOWEL.", letter);
+  } else {
+    printf("\n%c is a CONSONANT.", letter);
+  }
+
+  return 0;
 }
