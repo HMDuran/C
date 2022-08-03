df5aaccb2ed045f90e5690cdc9ad897db8c842ee
HMDuran <hanahmae.duran@gmail.com>
2022-08-03 15:34:04 +0800

    Update Lab_2

---
+++ Lab_2/LabTwo.c
@@ -0,0 +1,22 @@
+/**
+ * @file LabTwo.c
+ * @author HMDuran
+ * @brief program that will display hello wold, your name and your course
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+/* header file */
+#include <stdio.h>
+
+int main() {
+  /* display output */
+  printf("\t\t*********************************************************\n");
+  printf("\t\t\t\t\tHELLO WORLD!\n");
+  printf("\t\t\t\tMy Name is HM Duran -BSCS STUDENT\n");
+  printf("\t\t*********************************************************");
+  return 0;
+}
