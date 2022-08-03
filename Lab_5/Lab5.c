8b0eb3ee8acda89370f132b00ce10dd56b6f54b9
HMDuran <hanahmae.duran@gmail.com>
2022-08-03 15:55:32 +0800

    update Lab_3 & Lab_4

--- Lab_5/Lab5.c
+++ Lab_5/Lab5.c
@@ -1,22 +1,33 @@
-//identify if positive or negative number
-//identify if odd or even number
+/**
+ * @file Lab5.c
+ * @author HMDuran
+ * @brief
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+// identify if positive or negative number
+// identify if odd or even number
+
+/* header file */
 #include <stdio.h>
-int main()
-{
+int main() {
   int n1;
   printf("Enter a number : ");
   scanf("%d", &n1);
-  
-  if (n1>=0 && n1%2==0)
-  printf("%d is a Positive and Even Number",n1);
-  
-  else if(n1>=0 && n1%2 !=0)
-  printf("%d is a Postive and Odd Number", n1);
-  
-  else if(n1<=0 && n1%2 ==0)
-  printf("%d is a Negative and Even Number", n1);
-  
-  else
-  printf("%d is a Negative and Odd Number", n1);
 
+  if (n1 >= 0 && n1 % 2 == 0)
+    printf("%d is a Positive and Even Number", n1);
+
+  else if (n1 >= 0 && n1 % 2 != 0)
+    printf("%d is a Postive and Odd Number", n1);
+
+  else if (n1 <= 0 && n1 % 2 == 0)
+    printf("%d is a Negative and Even Number", n1);
+
+  else
+    printf("%d is a Negative and Odd Number", n1);
 }
