c896e56d0c4d240d01ce527bbf020cc302644543
HMDuran <hanahmae.duran@gmail.com>
2022-08-03 21:12:53 +0800

    Update Lab_9 & Lab_10

--- Lab_10/Lab10.c
+++ Lab_10/Lab10.c
@@ -1,34 +1,45 @@
-/** using one-dimensional array that calculates the sum of five 
-input values from the user and displays the calculated sum.  **/
+/**
+ * @file Lab10.c
+ * @author HMDuran
+ * @brief using one-dimensional array that calculates the sum of five input
+ * values from the user and displays the calculated sum.
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
+/* header file */
 #include <stdio.h>
-int main ()
-{
- int array[5];
- int i;
- char answer;
 
-do
-{ 
+int main() {
+  /* declaration of variables */
+  int array[5];
+  int i;
+  char answer;
+
+  do {
+    /* asking user to enter numbers  */
     printf("\nENTER YOUR 5 INTEGERS TO ADD\n");
 
     int sum = 0;
-    for(i=0; i<5; i++)
-{   
-    printf("\nEnter your no. %d: ", i+1);
-    scanf("%d", &array[i]);
-    sum += array[i];
-}
+    for (i = 0; i < 5; i++) {
+      printf("\nEnter your no. %d: ", i + 1);
+      scanf("%d", &array[i]);
+      /* calculate the sum of the entered numbers */
+      sum += array[i];
+    }
+    /* display ouput */
     printf("\nThe Sum Of All Entered Numbers: %d\n", sum);
 
+    /* Asking if the user want to repeat the program */
     printf("\nDO YOU WANT TO CONTINUE? (Y/N): ");
-        scanf(" %c", &answer);  
-     
-         if(answer == 'N' || answer == 'n'){
+    scanf(" %c", &answer);
 
-         printf("\nThank You For Using The Program!");
-        }
+    if (answer == 'N' || answer == 'n') {
+      /* display ouput if the use answer is n */
+      printf("\nThank You For Using The Program!");
     }
-    while (answer == 'Y' || answer == 'y');
+  } while (answer == 'Y' || answer == 'y');
 }
-
--- Lab_9/Lab9.c
+++ Lab_9/Lab9.c
@@ -1,36 +1,54 @@
-/**looping statement that will identify the even and odd numbers
-if even compute the sum from 1 to n. 
-if odd compute the factorial of n **/
+/**
+ * @file Lab9.c
+ * @author HMDuran
+ * @brief program that will identify the even and odd numbers if even compute
+ * the sum from 1 to n. if odd compute the factorial of n
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
+/* header file */
 #include <stdio.h>
-int main ()
-{
- int num, i,sum =0, f=1;
- char answer;
-    do
-    {
-      printf("\nEnter a number to determine if it is an even or odd number: ");   
-      scanf("%d", &num); 
-        if (num%2==0){
-            printf("\nThe number you entered %d is an even number",num);
-            for(i=1; i<=num;i++)
-            sum = sum + i;
-            printf("\nThe sum of the entered even number between 1 to %d is: %d\n", num, sum);
-        }
-        else {
-            printf("\nThe number you entered %d is an odd number",num);
-            for(i=1; i<=num;i++)
-            f = f * i;
-            printf("\nThe factorial of the entered odd number %d is: %d\n",num, f);
-        }
-        printf("\nDO YOU WANT TO CONTINUE? (Y/N): ");
-        scanf(" %c", &answer);  
-     
-         if(answer == 'N' || answer == 'n'){
 
-         printf("\nThank you for using the program!");
-        }
+int main() {
+  /* declaration of variables */
+  int num, i, sum = 0, f = 1;
+  char answer;
+
+  do {
+    /* asking user to enter number */
+    printf("\nEnter a number to determine if it is an even or odd number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+      /* display output */
+      printf("\nThe number you entered %d is an even number", num);
+      for (i = 1; i <= num; i++)
+        /* calculate the sum */
+        sum = sum + i;
+      /* display output */
+      printf("\nThe sum of the entered even number between 1 to %d is: %d\n",
+             num, sum);
+    } else {
+      /* display output */
+      printf("\nThe number you entered %d is an odd number", num);
+      for (i = 1; i <= num; i++)
+        /* calculate the factorial */
+        f = f * i;
+      /* display output */
+      printf("\nThe factorial of the entered odd number %d is: %d\n", num, f);
     }
-    while (answer == 'Y' || answer == 'y');
-return 0;
+    /* Asking if the user want to repeat the program */
+    printf("\nDO YOU WANT TO CONTINUE? (Y/N): ");
+    scanf(" %c", &answer);
+
+    if (answer == 'N' || answer == 'n') {
+      /* display ouput if the use answer is n */
+      printf("\nThank you for using the program!");
+    }
+  } while (answer == 'Y' || answer == 'y');
+  return 0;
 }
