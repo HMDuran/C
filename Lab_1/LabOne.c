bf1b3cab59516d237819af8e6d1ddd30bb268a39
HMDuran <hanahmae.duran@gmail.com>
2022-08-03 15:14:01 +0800

    Update Lab_1

--- Lab_1/LabOne.c
+++ Lab_1/LabOne.c
@@ -1,18 +1,36 @@
-#include<stdio.h>
-int main()
-{   
-    int num1, num2;
-    
-    printf("Enter First Number: \n");
-    scanf("%d", &num1);
-    printf("Enter Second Number: \n");
-    scanf("%d", &num2);
+/**
+ * @file LabOne.c
+ * @author HMDuran
+ * @brief  program that will display the sum, difference, product, quotient and
+ * modulus of two numbers
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
-    printf("The sum of two numbers is: %d\n", num1 + num2);
-    printf("The difference of two numbers is: %d\n", num1 - num2);
-    printf("The product of two numbers is: %d\n", num1 * num2);
-    printf("The quotient of two numbers is: %d\n", num1 / num2);
-    printf("The modulus of two numbers is: %d\n", num1 % num2);
+/* header files */
+#include <stdio.h>
 
-    return 0;
+int main() {
+  /* declaration of variables */
+  int firstNum, secondNum;
+
+  /* asking user first number*/
+  printf("\nEnter First Number: ");
+  scanf("%d", &firstNum);
+
+  /* asking user second number */
+  printf("Enter Second Number: ");
+  scanf("%d", &secondNum);
+
+  /*calculate and display the output */
+  printf("\nThe sum of two numbers is: %d\n", firstNum + secondNum);
+  printf("The difference of two numbers is: %d\n", firstNum - secondNum);
+  printf("The product of two numbers is: %d\n", firstNum * secondNum);
+  printf("The quotient of two numbers is: %d\n", firstNum / secondNum);
+  printf("The modulus of two numbers is: %d\n", firstNum % secondNum);
+
+  return 0;
 }
