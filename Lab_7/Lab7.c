c9603cee2eba922fc895b3a338443a127ff13840
HMDuran <hanahmae.duran@gmail.com>
2022-08-03 21:03:38 +0800

    Update Lab_7 & Lab_8

--- Lab_7/Lab7.c
+++ Lab_7/Lab7.c
@@ -1,26 +1,41 @@
-//looping statement that will calculate the sum of the given number
-#include <stdio.h>
-int main ()
-{
- int num, sum = 0, i;
- char answer;
-    do
-    {
-      printf("\nEnter a number to find the sum: ");
-      scanf("%d", &num);  
-      sum=0;
-     for( i=1; i<=num; i++){
-      sum +=i; 
-}
-      printf("The sum of the given %d is: %d\n", num, sum);
-        
-     printf("\nDO YOU WANT TO CONTINUE? (Y/N): ");
-    scanf(" %c", &answer);  
-     
-     if(answer == 'N' || answer == 'n'){
+/**
+ * @file Lab7.c
+ * @author HMDuran
+ * @brief program that will calculate the sum of the given number
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
-     printf("\nThank you for using the program!");
-        }
-    }while (answer == 'Y' || answer == 'y');
-return 0;
+/* header file */
+#include <stdio.h>
+
+int main() {
+  /* declaration of variables */
+  int userNumber, sum = 0, i;
+  char answer;
+
+  do {
+    /* asking user to enter number */
+    printf("\nEnter a number to find the sum: ");
+    scanf("%d", &userNumber);
+    sum = 0;
+    for (i = 1; i <= userNumber; i++) {
+      sum += i;
+    }
+    /* display ouput */
+    printf("\nThe sum of the given %d is: %d\n", userNumber, sum);
+
+    /* Asking if the user want to repeat the program */
+    printf("\n\nDO YOU WANT TO CONTINUE? (Y/N): ");
+    scanf(" %c", &answer);
+
+    if (answer == 'N' || answer == 'n') {
+      /* display ouput if the use answer is n */
+      printf("\nThank you for using the program!");
+    }
+  } while (answer == 'Y' || answer == 'y');
+  return 0;
 }
--- Lab_8/Lab8.c
+++ Lab_8/Lab8.c
@@ -1,31 +1,48 @@
-//looping statement that will identify the even and odd numbers for the given number
-#include <stdio.h>
-int main ()
-{
- int num, i;
- char answer;
-    do
-    {
-      printf("\nEnter a number: ");   
-      scanf("%d", &num); 
-      printf("The Even Numbers are: ");
-         for(i=2; i<=num; i++){
-            if (i%2==0)
-               printf(" %d", i);
-     }
-      printf("\nThe Odd Numbers are: ");   
-         for (i=1; i<=num; i++){
-            if (i%2!=0)
-               printf(" %d", i); 
-     }  
-     printf("\nDO YOU WANT TO CONTINUE? (Y/N): ");
-     scanf(" %c", &answer);  
-     
-         if(answer == 'N' || answer == 'n'){
+/**
+ * @file Lab8.c
+ * @author HMDuran
+ * @brief program that will identify the even and odd numbers for the given
+ * number
+ * @version 0.1
+ * @date 2022-08-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
-         printf("\nThank you for using the program!");
-        }
+/* header file */
+#include <stdio.h>
+
+int main() {
+  /* declaration of variables */
+  int userNumber, i;
+  char answer;
+
+  do {
+    /* asking user to enter number */
+    printf("\nEnter a number: ");
+    scanf("%d", &userNumber);
+
+    /* display ouput */
+    printf("\nThe Even Numbers are: ");
+    for (i = 2; i <= userNumber; i++) {
+      if (i % 2 == 0)
+        printf(" %d", i);
     }
-    while (answer == 'Y' || answer == 'y');
-return 0;
+    /* display ouput */
+    printf("\nThe Odd Numbers are: ");
+    for (i = 1; i <= userNumber; i++) {
+      if (i % 2 != 0)
+        printf(" %d", i);
+    }
+    /* Asking if the user want to repeat the program */
+    printf("\n\nDO YOU WANT TO CONTINUE? (Y/N): ");
+    scanf(" %c", &answer);
+
+    if (answer == 'N' || answer == 'n') {
+      /* display ouput if the use answer is n */
+      printf("\nThank you for using the program!");
+    }
+  } while (answer == 'Y' || answer == 'y');
+  return 0;
 }
